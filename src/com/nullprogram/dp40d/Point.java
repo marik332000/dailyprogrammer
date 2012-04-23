493b35888ccd1caa30dda17c702c3f91e61318e6
Christopher Wellons <mosquitopsu@gmail.com>
2012-04-23 10:29:27 -0400

    Make Point truly immutable.

--- src/com/nullprogram/dp40d/Point.java
+++ src/com/nullprogram/dp40d/Point.java
@@ -1,17 +1,31 @@
 package com.nullprogram.dp40d;
 
+import java.util.Arrays;
 import java.util.Random;
 import lombok.Data;
 
 @Data
-class Point {
+public final class Point {
     private final float[] coords;
 
+    /**
+     * Randomly generate a new point.
+     * @param rng   the random number generator to use
+     * @param dims  the number of dimensions for this point
+     */
     Point(Random rng, int dims) {
         coords = new float[dims];
         for (int i = 0; i < dims; i++) {
             coords[i] = rng.nextFloat();
         }
+    }
+
+    /**
+     * Get this point's coordinates.
+     * @return a copy of this point's coordinate array
+     */
+    public float[] getCoords() {
+        return Arrays.copyOf(coords, coords.length);
     }
 
     /**
