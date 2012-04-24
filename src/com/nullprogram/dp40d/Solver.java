61093e71f8a91b22d4c1e9fa68862bd84de97bd3
Christopher Wellons <mosquitopsu@gmail.com>
2012-04-24 12:34:45 -0400

    Expand on the Solver Javadoc a little.

--- src/com/nullprogram/dp40d/Solver.java
+++ src/com/nullprogram/dp40d/Solver.java
@@ -1,7 +1,9 @@
 package com.nullprogram.dp40d;
 
 /**
- * Implementers of this interface solve the closest pair problem.
+ * Implementers of this interface solve the closest pair
+ * problem. <i>No</i> computational work (sorting, etc.) should be
+ * done in the constructor, just assignments and invariant checks.
  */
 public interface Solver {
 
